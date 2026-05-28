f9a8a46efa9815144520594335f587173daeb1d1
sanojamat2017 <sanojamat2009@gmail.com>
2026-05-28 12:41:52 +0530

    first changes 2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-// import { StrictMode } from 'react'
-=======
->>>>>>> cb509db8a4628bba873cc794b0ca892198759af0
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>,
-=======
-    <App />,
->>>>>>> cb509db8a4628bba873cc794b0ca892198759af0
+  </StrictMode>,
 )
